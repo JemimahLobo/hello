a3661f7149e3a644a3c866f9d82de6b15c6b41bc
Jemimah Lobo <jlobo3@huskers.unl.edu>
2024-09-03 20:57:39 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Jemimah Lobo
+ *         jlobo3@unl.edu
+ * Date: 2024/09/03
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I love the School of Computing!\n");
+
+  return 0;
+}
